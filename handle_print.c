7a7b25d1bf4708475ac69bff93397c76f02ca563
amanabay <117741379+amanabay@users.noreply.github.com>
2023-01-24 14:11:41 +0300

    update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -6,7 +6,7 @@
  * @ind: index.
  * @buffer: buffer array to handle print.
  * @flags: calculates the active active flags
- * @width: get width.
+ * @width: get width
  * @precision: precision specification
  * @size: size specifier
  * Return: 1 or 2;
