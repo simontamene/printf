ebd3b7b0e972ef6a4c03f78607e952c708b8224b
amanabay <117741379+amanabay@users.noreply.github.com>
2023-01-24 19:32:38 +0300

    buffer sizing fix

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - final printf function
+ * @format: format
+ * Return: printed chars
  */
 int _printf(const char *format, ...)
 {
@@ -26,7 +26,6 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -53,9 +52,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_buffer - prints the contents of the buffer, if exists
+ * @buffer: array of chars
+ * @buff_ind: index at which to add next char, representing the length
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
